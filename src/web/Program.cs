effd3b7aa22b402c70f2d3aee50cf5eb417ee3d2
Geoff Stockham <geoff@stormid.com>
2016-08-16 14:02:41 +0100

    Start two load-balanced web sites

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace WebApplication
 {
@@ -11,7 +12,13 @@ namespace WebApplication
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
